12f34e446a42b3c0a47f969a368e1921185c0e36
Sarah Besserer <sarah.besserer@brutifulgames.com>
2020-01-04 17:25:20 -0600

    Revert "Merge branch 'master' of https://github.com/godjammit/murdermoons"
    This reverts commit 81d8cb34d3d9058d0f78d465978d32a75a0f2205, reversing
    changes made to 1605a097debd30dd74ac4c0b49226d979d263f4d.

--- Murder Moon/Assets/Scripts/Projectile.cs
+++ Murder Moon/Assets/Scripts/Projectile.cs
@@ -1,57 +1,18 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-	public bool IsHeld { get; internal set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-	Yeeter _lastYeeter;
-	float _lastHeldTimestamp;
-
-	public Rigidbody2D MyRigidbody;
-	public Collider2D MyCollider;
-
-	const float HitOriginDelay = 0.18f; //3 frames at 30fps
-
-	internal void Grab(Yeeter yeeter)
-	{
-		IsHeld = true;
-		_lastYeeter = yeeter;
-		MyCollider.enabled = false;
-		MyRigidbody.simulated = false;
-	}
-
-	internal void Release()
-	{
-		IsHeld = false;
-		_lastHeldTimestamp = Time.time;
-		MyCollider.enabled = true;
-		MyRigidbody.simulated = true;
-		StartCoroutine(MakeInvulnerabale());
-	}
-
-	IEnumerator MakeInvulnerabale()
-	{
-		Collider2D[] originColliders = _lastYeeter.GetComponentsInChildren<Collider2D>();
-		foreach (var collider in originColliders)
-		{
-			Physics2D.IgnoreCollision(collider, this.MyCollider, true);
-		}
-
-		yield return new WaitForSeconds(HitOriginDelay);
-
-		foreach (var collider in originColliders)
-		{
-			Physics2D.IgnoreCollision(collider, this.MyCollider, false);
-		}
-	}
-
-	private void OnCollisionEnter2D(Collision2D collision)
-	{
-		if (IsHeld == true)
-			return;
-		Debug.Log("hit");
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
