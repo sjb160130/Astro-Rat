b6ebf84b48bcefa72f8a3b8880e6e7b57ea22573
Tyler Tomaseski <TylerTomaseski@gmail.com>
2020-01-04 00:48:11 -0600

    Core infrastructure and odin inspector

---
+++ Murder Moon/Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+internal class SpawnPoint
+{
+	public int PlayerID { get { return _playerID; } }
+	[SerializeField]
+	int _playerID;
+
+	[SerializeField]
+	GameObject _playerPrefab;
+
+	RatPlayer _spawendPlayer;
+
+	public void Spawn()
+	{
+		
+	}
+}
---
+++ Murder Moon/Assets/Scripts/StateMachine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class StateMachine<T> : MonoBehaviour where T : IConvertible, IComparable
+{
+	protected T _currentState { get; private set; }
+
+	protected void SetState(T newState)
+	{
+		if (newState.CompareTo(_currentState) == 0)
+			return;
+		ExitState(_currentState);
+		_currentState = newState;
+		EnterState(_currentState);
+	}
+
+	abstract protected void ExitState(T previousState);
+
+	abstract protected void EnterState(T nextState);
+}
