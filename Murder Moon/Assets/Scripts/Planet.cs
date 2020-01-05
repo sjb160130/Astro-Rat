5c185cceba89de6f09f33c54d8fc815102651e54
Tyler Tomaseski <TylerTomaseski@gmail.com>
2020-01-05 11:06:39 -0600

    set sprites to sprite import type, setup planet prefab, and created simple gravity get utility

---
+++ Murder Moon/Assets/Scripts/Planet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+	public static Vector2 GetSimpleGravityDirection(Collider2D collider, out Planet closestPlanet)
+	{
+		var planets = GameObject.FindGameObjectsWithTag("Planet");
+		LayerMask lm = LayerMask.NameToLayer("Planet");
+		float closestDistance = float.MaxValue;
+		GameObject closestPlanetGO = null;
+		foreach (var p in planets)
+		{
+			Collider2D planetCollider = p.GetComponent<Collider2D>();
+			float dist = planetCollider.Distance(collider).distance;
+			if (dist < closestDistance)
+			{
+				closestPlanetGO = p;
+				closestDistance = dist;
+			}
+		}
+
+		Vector2 gravityDirection = Vector2.down;
+		if (closestPlanetGO != null)
+		{
+			gravityDirection = (closestPlanetGO.transform.position - collider.transform.position).normalized;
+			closestPlanet = closestPlanetGO.GetComponent<Planet>();
+		}
+		else
+			closestPlanet = null;
+
+		return gravityDirection;
+	}
+}
