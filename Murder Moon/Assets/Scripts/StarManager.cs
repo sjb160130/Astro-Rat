df87e6734ba10a562eae71a16cf3bf6a58044499
Summer <summer.d.howard@gmail.com>
2020-01-06 21:25:53 -0600

    object pooling started

---
+++ Murder Moon/Assets/Scripts/StarManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using PKG;
+using Random = UnityEngine.Random;
+
+[Serializable]
+public class StarSpawnPoints
+{
+    [SerializeField]
+    public Vector3 SpawnHere;
+    [SerializeField]
+    public Vector2 DirectionToShoot;
+}
+
+public class StarManager : MonoBehaviour
+{
+    public GameObject star;
+    private PoolManager _pool;
+
+
+    public List<StarSpawnPoints> spawnPoints;
+
+
+
+    void Start()
+    {
+        _pool = GetComponent<PoolManager>();
+        StartCoroutine(Spawn(0.1f));
+    }
+
+
+    private IEnumerator Spawn(float waitTime)
+    {
+        _pool.spawnObject(star);
+        _pool.PrintStatus();
+
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+
+            StarSpawnPoints location = getRandomSpawnLocation();
+
+            var star2 = _pool.spawnObject(star, location.SpawnHere, Quaternion.identity);
+            star2.GetComponent<Rigidbody2D>().AddForce(location.DirectionToShoot);
+        }
+    }
+
+
+    private StarSpawnPoints getRandomSpawnLocation()
+    {
+        var index = Random.Range(0, spawnPoints.Count);
+        index = (index -1) < 0 ? 0 : (index - 1);
+        return spawnPoints[index];
+    }
+}
+
