96b88863b278af2af2174ea33bd399b039003a43
Summer <summer.d.howard@gmail.com>
2020-01-12 17:47:00 -0600

    star AOEs kill player

---
+++ Murder Moon/Assets/Scripts/StarAOE.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class StarAOE : MonoBehaviour
+{
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        //AudioManager.Instance.PlaySound(soundWhenHittingSomething, transform.position);
+
+        if (collision.collider.CompareTag("Player"))
+        {
+            collision.collider.GetComponent<RatPlayer>().Kill();
+        }
+    }
+}
